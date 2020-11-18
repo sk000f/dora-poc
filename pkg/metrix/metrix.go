3e69e9b802e629d3e8dec3ce728eec128d96a9f7
mikeskaife <mike@skaife.me>
2020-11-18 20:55:27 +0000

    initial main app startup with config

--- pkg/metrix/metrix.go
+++ pkg/metrix/metrix.go
@@ -2,12 +2,27 @@ package metrix
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/sk000f/metrix/pkg/collector/gitlab"
 )
 
 // Start initialises and configures the application
 func Start(cfg *Config) {
 	fmt.Println(cfg.GitlabURL)
 	fmt.Println(cfg.GitlabToken)
+
+	client, err := gitlab.SetupClient(cfg.GitlabToken, cfg.GitlabURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	projects, _, err := client.Projects.ListProjects(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(projects)
 }
 
 // Config stores configuration values
