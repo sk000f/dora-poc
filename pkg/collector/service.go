b2d86d7e99897c7205db338a3dcc0160cbf2d43e
sk000f <mike@skaife.me>
2020-11-23 20:34:22 +0000

    specification for generic metric data collector service

---
+++ pkg/collector/service.go
@@ -0,0 +1,28 @@
+package collector
+
+// Service provides functionality for updating CI data
+type Service struct {
+	ci CIServer
+	r  Repository
+}
+
+// CIServer provides functionality for getting data from CI server
+type CIServer interface {
+	GetUpdatedData()
+}
+
+// Repository provides access to data storage
+type Repository interface {
+	SaveData()
+}
+
+// NewService creates a collector with required dependencies
+func NewService(ci CIServer, r Repository) *Service {
+	return &Service{ci, r}
+}
+
+// RefreshData collects data from CI server and saves in data repository
+func (s *Service) RefreshData() {
+	s.ci.GetUpdatedData()
+	s.r.SaveData()
+}
