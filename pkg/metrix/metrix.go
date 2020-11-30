1892560153b28029a1916d7758bcb059f1d3e843
sk000f <mike@skaife.me>
2020-11-30 19:03:09 +0000

    add dotenv for config, replace mock repo with Mongo

--- pkg/metrix/metrix.go
+++ pkg/metrix/metrix.go
@@ -2,11 +2,14 @@ package metrix
 
 import (
 	"fmt"
+	"log"
 	"os"
 
-	"github.com/sk000f/metrix/pkg/collector"
+	"github.com/joho/godotenv"
 
+	"github.com/sk000f/metrix/pkg/collector"
 	"github.com/sk000f/metrix/pkg/collector/gitlab"
+	"github.com/sk000f/metrix/pkg/storage/mongodb"
 )
 
 // Start initialises and configures the application
@@ -19,7 +22,8 @@ func Start() error {
 		URL:   cfg.GitLabURL,
 	}
 
-	r := new(mockRepo)
+	r := new(mongodb.DB)
+	r.ConnStr = cfg.DBConnString
 
 	c := collector.NewService(gl, r)
 
@@ -34,31 +38,24 @@ func Start() error {
 
 // SetupConfig configures application based on environment variables
 func SetupConfig() *Config {
+
 	cfg := new(Config)
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	cfg.GitLabURL = os.Getenv("METRIX_GITLAB_URL")
 	cfg.GitLabToken = os.Getenv("METRIX_GITLAB_TOKEN")
+	cfg.DBConnString = os.Getenv("METRIX_DB_CONN_STRING")
 
 	return cfg
 }
 
 // Config stores configuration values
 type Config struct {
-	GitLabURL   string
-	GitLabToken string
-}
-
-type mockRepo struct {
-	ProjectData    []*collector.Project
-	DeploymentData []*collector.Deployment
-}
-
-func (m *mockRepo) SaveProjects(p []*collector.Project) {
-	for _, proj := range p {
-		m.ProjectData = append(m.ProjectData, proj)
-	}
-}
-
-func (m *mockRepo) SaveDeployment(d *collector.Deployment) {
-	m.DeploymentData = append(m.DeploymentData, d)
+	GitLabURL    string
+	GitLabToken  string
+	DBConnString string
 }
