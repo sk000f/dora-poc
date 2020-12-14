a85d5cfe0b57cff02cd10b4c64da6f3aaac98621
sk000f <mike@skaife.me>
2020-12-14 18:19:02 +0000

    resolve gofmt issue

--- pkg/http/graphql/graph/resolver.go
+++ pkg/http/graphql/graph/resolver.go
@@ -4,4 +4,5 @@ package graph
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
+// Resolver type representing resolvers in server implementation
 type Resolver struct{}
